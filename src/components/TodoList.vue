e2d1ee5f3c6cdb3edfdafa1801dc45ecd5672d06
Cuzo Miao <ascz123j@gmail.com>
2025-04-09 14:49:41 +0800

    feat: part of todo detail design

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,17 +1,33 @@
 <template>
   <q-list bordered>
-    <q-item v-ripple v-for="todo in todoList" :key="todo.id">
-      <q-item-section avatar>
-        <q-checkbox
-          :model-value="todo.completed"
-          @update:model-value="_onTodoCompletedChange(todo, $event)"
-        />
-      </q-item-section>
-      <q-item-section>{{ todo.title }}</q-item-section>
-      <q-item-section side>
-        <q-btn round dense flat icon="delete" @click="deleteTodo(todo)" />
-      </q-item-section>
-    </q-item>
+    <q-expansion-item v-for="todo in todoList" :key="todo.id" hide-expand-icon>
+      <template #header>
+        <q-item-section avatar>
+          <q-checkbox
+            :model-value="todo.completed"
+            @update:model-value="_onTodoCompletedChange(todo, $event)"
+          />
+        </q-item-section>
+        <q-item-section>{{ todo.title }}</q-item-section>
+        <!-- <q-item-section side>
+          <q-btn round dense flat icon="delete" @click="deleteTodo(todo)" />
+        </q-item-section> -->
+      </template>
+      <template #default>
+        <div class="q-pa-sm" v-if="todo.completed">
+          <span class="text-grey q-mr-sm">Finished at</span>
+          <p>{{ todo.finishedAt }}</p>
+        </div>
+        <div class="q-pa-sm" v-if="todo.description">
+          <span class="text-grey q-mr-sm">Description</span>
+          <p>{{ todo.description }}</p>
+        </div>
+        <footer class="todo__footer q-pa-sm text-grey">
+          <q-btn icon="edit" flat dense round />
+          <q-btn icon="delete" flat dense round @click="deleteTodo(todo)" />
+        </footer>
+      </template>
+    </q-expansion-item>
   </q-list>
 </template>
 <script setup lang="ts">
@@ -37,3 +53,10 @@ function deleteTodo(todo: Todo) {
   TodoStore.remove(todo);
 }
 </script>
+<style lang="scss">
+.todo__footer {
+  display: flex;
+  justify-content: flex-end;
+  gap: 8px
+}
+</style>
